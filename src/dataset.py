c4aceee533cdac8d25ee113e5e1b15718e3773da
plestz <96513494+plestz@users.noreply.github.com>
2025-01-02 23:37:03 -0500

    feat: functional synthetic CNN at 90%+ test accuracy, docs: all functions now have docstrings

--- src/dataset.py
+++ src/dataset.py
@@ -8,16 +8,38 @@ import os
 from enum import Enum
 
 class DSubset(Enum):
+    """
+    Indicator for whether ImageDataset should contain training
+    or test instances.
+    """
     TRAIN = 1
     TEST = 2
 
 class Label(Enum):
+    """
+    Indicator for whether ImageDataset should have instance labels be 
+    the object within the image OR whether or not the image was synthetically
+    generated.
+    """
     OBJECT = 1 # Target label is object in the image (10 options)
     REAL_OR_SYNTHETIC = 2 # Target label is whether the image is real or synthetically generated (2 options)
 
 class ImageDataset(Dataset):
-
+    """
+    A custom dataset class designed to contain and produce examples originating
+    from the CIFAKE (https://www.kaggle.com/datasets/birdy654/cifake-real-and-ai-generated-synthetic-images/data)
+    dataset.
+    """
     def __init__(self, split: DSubset, label_type: Label, transform = None, target_transform = None):
+        """
+        Initializer for ImageDataset.
+
+        Args:
+            split -- An indicator for the type of examples contained, see DSubset enum.
+            label_type -- An indicator for the type of instance label, see Label enum.
+            transform -- The torch transform to be exacted on the image.
+            target_transform -- The torch transform to be exacted on the label.
+        """
 
         assert isinstance(split, DSubset)
         assert isinstance(label_type, Label)
@@ -37,9 +59,25 @@ class ImageDataset(Dataset):
         self.target_transform = target_transform
 
     def __len__(self):
+        """
+        Provides the number of datapoints contained within the ImageDatset.
+
+        Examples originate from the REAL and FAKE subdirectories, making their
+        sum for the designated DSubset the total dataset length.
+        """
         return self.num_real_instances + self.num_synthetic_instances
 
     def __getitem__(self, index):
+        """
+        Produces the item from the ImageDataset specified by index.
+
+        Args:
+            index -- Indicates which instance to produce.
+
+        Returns:
+            X -- Image features (i.e. pixels)
+            y -- Image label
+        """
 
         if index < 0 or index >= self.__len__():
             raise IndexError(f'Provided index out of range of number of files available in {self.split} split.')
@@ -70,7 +108,7 @@ class ImageDataset(Dataset):
         elif self.label_type == Label.REAL_OR_SYNTHETIC:
             label = 0 if image_type == 'REAL' else 1 # (0 = Real, 1 = Synthetic)
         else:
-            raise ValueError('No proper label type provided.')
+            raise ValueError(f'No proper label type provided, was {self.label_type}')
         
         if self.transform:
             image = self.transform(image)
@@ -81,6 +119,28 @@ class ImageDataset(Dataset):
 
 
 def get_channel_means_stdevs(dataloader: DataLoader, num_channels = 1, verbose = False) -> tuple[tuple[float], tuple[float]]:
+    """
+    Given the dataset contained within the dataloader, produces the pixel
+    value means and standard deviations for each channel.
+
+    Note:
+        - torch.mean was considered, but torch.sum was used in tandem with
+        the growing total_pixels to ensure that the final dataloader batch
+        (often of a size smaller than batch_size) is properly averaged into
+        the final result. Using torch.mean when including this last batch
+        would overly weight those examples when channel_means = mean(batch_mean)
+        in this scenario.
+
+    Args:
+        dataloader -- Links to the dataset containing the relevant images.
+        num_channels -- The number of channels to be averaged into. Must be
+        equal to the number of channels in the images in dataloader.
+        verbose -- Indicates whether or not to print image process counts.
+
+    Returns:
+        tuple_1 -- pixel value means for each channel
+        tuple_2 -- pixel value standard deviations for each channel
+    """
 
     cumulative_sum = torch.zeros(num_channels, dtype = torch.float32) # RGB
     cumulative_squared_sum = torch.zeros(num_channels, dtype = torch.float32) # RGB
@@ -102,4 +162,3 @@ def get_channel_means_stdevs(dataloader: DataLoader, num_channels = 1, verbose =
     channel_stdevs = torch.sqrt(channel_variances)
 
     return tuple(channel_means.tolist()), tuple(channel_stdevs.tolist())
-
